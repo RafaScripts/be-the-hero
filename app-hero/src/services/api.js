5e07ac333f9a30405fda7e77033895bbf7fb6b6b
Walker665 <rafaelfreire0@gmail.com>
2020-03-28 16:45:46 -0300

    build android apk

--- app-hero/src/services/api.js
+++ app-hero/src/services/api.js
@@ -3,7 +3,7 @@ import 'intl';
 import 'intl/locale-data/jsonp/pt-BR';
 
 const api = axios.create({
-    baseURL: 'http://192.168.0.19:3333'
+    baseURL: 'http://c65e4463.ngrok.io'
 });
 
 export default api;
